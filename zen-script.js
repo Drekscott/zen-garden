d4b6c5f77fa03caf55a8ca61ccea2f4fdfedd23a
marcusshepp <marcus.shepherd@outlook.com>
2014-09-02 15:53:28 -0400

    jquery

--- zen-script.js
+++ zen-script.js
@@ -5,58 +5,135 @@
 
 	$('h3:contains(Resources)').click(function(){
 
-		$('.view-css, .css-resources, .zen-faq, .zen-submit, .zen-translations, header > h2').slideToggle(1000);
+		$('.view-css, .css-resources, .zen-faq, .zen-submit, .zen-translations, header > h2').fadeToggle(500);
+
+	});
+
+	$('h3:contains(Resources)').click(function(){
+
+
+		$('h3:contains(Resources)').html('Click to Hide');
+	
 
 	});
 
 	$('h3:contains(Archives)').click(function(){
 
-		$('.next, .viewall').slideToggle(1000);
+		$('.next, .viewall').slideToggle(500);
+
+	});
+
+	$('h3:contains(Archives)').click(function(){
+
+
+		$('h3:contains(Archives)').html('Click to Hide');
+	
 
 	});
 
 	$('h3:contains(Requirements)').click(function(){
 
-		$('.requirements > p').slideToggle(1000);
+		$('.requirements > p').slideToggle(500);
+
+	});
+
+	$('h3:contains(Requirements)').click(function(){
+
+
+		$('h3:contains(Requirements)').html('Click to Hide');
+	
 
 	});
 
 	$('h3:contains(Benefits)').click(function(){
 
-		$('.benefits > p').slideToggle(1000);
+		$('.benefits > p').slideToggle(500);
+
+	});
+
+	$('h3:contains(Benefits)').click(function(){
+
+
+		$('h3:contains(Benefits)').html('Click to Hide');
+	
 
 	});
 
 	$('h3:contains(Participation)').click(function(){
 
-		$('.participation > p').slideToggle(1000);
+		$('.participation > p').slideToggle(500);
+
+	});
+
+	$('h3:contains(Participation)').click(function(){
+
+
+		$('h3:contains(Participation)').html('Click to Hide');
+	
 
 	});
 
 	$('h3:contains(So What is This About?)').click(function(){
 
-		$('.explanation > p').slideToggle(1000);
+		$('.explanation > p').slideToggle(500);
+
+	});
+
+	$('h3:contains(So What is This About?)').click(function(){
+
+
+		$('h3:contains(So What is This About?)').html('Click to Hide');
+	
+	});
+
+	$('h3:contains(The Road to Enlightenment)').click(function(){
+
+		$('.preamble > p').slideToggle(500);
 
 	});
 
 	$('h3:contains(The Road to Enlightenment)').click(function(){
 
-		$('.preamble > p').slideToggle(1000);
-
+		$('h3:contains(The Road to Enlightenment)').html('Click to Hide');
+	
 	});
 
 	$('h1:contains(CSS Zen Garden)').click(function(){
 
-		//$('.summary > p').slideToggle(1000);
-		//$('a:contains(html file), a:contains(css file)').fadeIn( "slow" );
-		$( "div > p" ).fadeToggle( "slow" );
+		$( ".summary > p" ).slideToggle(500);
+	
 	});
+
+	$('h1:contains(CSS Zen Garden)').click(function(){
+
+
+		$('h1:contains(CSS Zen Garden)').html('Click to Hide');
+	
+
+	});
+
+	$('h1:contains(Click to Hide)').click(function(){
+
+
+		$('h1:contains(Click to Hide)').html('CSS Zen Garden');
+	
+		
+	});
+
 
 	$('h3:contains(Select a Design:)').click(function(){
 
 //		$("a[href~='/221/']").slideToggle(500);
-		$( '.designer-name, .design-name, li:contains(by)').slideToggle(1000)
+		$( '.designer-name, .design-name, li:contains(by)').slideToggle(500);
 //		$( this ).toggleClass("h3");
+
+	});
+
+	$('h3:contains(Select a Design:)').click(function(){
+
+
+		$('h3:contains(Select a Design:)').html('Click to Show');
+	
 
 	});
 
